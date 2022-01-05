20a207fed19ca89bf38db2a6acf7c519c564550e
Ronnie Katunga <bit-031-17@must.ac.mw>
2022-01-05 19:50:51 +0200

    Update footer.php

--- application/views/web/inc/footer.php
+++ application/views/web/inc/footer.php
@@ -6,31 +6,31 @@
             <div class="col_1_of_4 span_1_of_4">
                 <h4>Information</h4>
                 <ul>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Customer Service</a></li>
-                    <li><a href="#"><span>Advanced Search</span></a></li>
-                    <li><a href="#">Orders and Returns</a></li>
-                    <li><a href="#"><span>Contact Us</span></a></li>
+                    <li><a href="contact">About Us</a></li>
+                    <li><a href="contact">Customer Service</a></li>
+                    <li><a href="contact"><span>Advanced Search</span></a></li>
+                    <li><a href="contact">Orders and Returns</a></li>
+                    <li><a href="contact"><span>Contact Us</span></a></li>
                 </ul>
             </div>
             <div class="col_1_of_4 span_1_of_4">
                 <h4>Why buy from us</h4>
                 <ul>
-                    <li><a href="about.html">About Us</a></li>
-                    <li><a href="faq.html">Customer Service</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="contact.html"><span>Site Map</span></a></li>
+                    <li><a href="contact">About Us</a></li>
+                    <li><a href="contact">Customer Service</a></li>
+                    <li><a href="contact">Privacy Policy</a></li>
+                    <li><a href="contact"><span>Site Map</span></a></li>
                     <li><a href="preview.html"><span>Search Terms</span></a></li>
                 </ul>
             </div>
             <div class="col_1_of_4 span_1_of_4">
                 <h4>My account</h4>
                 <ul>
-                    <li><a href="contact.html">Sign In</a></li>
-                    <li><a href="index.html">View Cart</a></li>
-                    <li><a href="#">My Wishlist</a></li>
-                    <li><a href="#">Track My Order</a></li>
-                    <li><a href="faq.html">Help</a></li>
+                    <li><a href="customer/login">Sign In</a></li>
+                    <li><a href="cart">View Cart</a></li>
+                    <li><a href="cart">My Wishlist</a></li>
+                    <li><a href="contact">Track My Order</a></li>
+                    <li><a href="contact">Help</a></li>
                 </ul>
             </div>
             <div class="col_1_of_4 span_1_of_4">
