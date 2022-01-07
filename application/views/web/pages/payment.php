1d805d7ae052aec668798669bb041278f5d2a966
Brisso01 <bit-036-17@must.ac.mw>
2022-01-07 20:29:03 +0200

    Revert "Update payment.php"
    This reverts commit 8d5f01c5e96a1b46eb8da32827383e18861b59c2.

--- application/views/web/pages/payment.php
+++ application/views/web/pages/payment.php
@@ -5,7 +5,6 @@
         <div class="register_account" style="text-align:center;display:inline-block;float: none">
             <h3>Payment Options</h3>
             <p>Your Order Is Sucessfully Done.</p>
-            <p>Thank you!!.</p>
            
         </div>  	
         <div class="clear"></div>
