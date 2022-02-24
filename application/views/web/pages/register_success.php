e1bcc29ed66cd6f7b6d03685029cfb4d6f4c231d
Brisso01 <bit-036-17@must.ac.mw>
2022-02-24 11:50:08 +0200

    Update register_success.php

--- application/views/web/pages/register_success.php
+++ application/views/web/pages/register_success.php
@@ -3,11 +3,11 @@
 <div class="main">
     <div class="content" style="text-align: center">
         <div class="register_account" style="text-align:center;display:inline-block;float: none">
-            <h3>Welcome To <?php echo $this->session->flashdata('customer_name'); ?></h3>
+            <h3>Welcome To Nsikawanga</h3>
             
-            <p>Hi <?php echo $this->session->flashdata('customer_name'); ?>, You Successfully Registration Our Site
-                Please Go Your Email <a href="mailto:<?php echo $this->session->flashdata('customer_email'); ?>"><b><?php echo $this->session->flashdata('customer_email'); ?></b></a>
-                And Active Your Account . Thank You Stay With Us.
+            <p>Hi <?php echo $this->session->flashdata('customer_name'); ?>, You have Successfully Registered on Our Site
+                Please check Your Email <a href="mailto:<?php echo $this->session->flashdata('customer_email'); ?>"><b><?php echo $this->session->flashdata('customer_email'); ?></b></a>
+                And Activate Your Account . Shop With Us.
             </p>
             
             
