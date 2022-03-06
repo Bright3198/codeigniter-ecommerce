ade03cf9fc8c25ed8cd1431a3b41c85decca8cec
Bright3198 <94518961+Bright3198@users.noreply.github.com>
2022-03-06 17:25:19 +0200

    Update register_success.php

--- application/views/web/pages/register_success.php
+++ application/views/web/pages/register_success.php
@@ -4,15 +4,15 @@
     <div class="content" style="text-align: center">
         <div class="register_account" style="text-align:center;display:inline-block;float: none">
             <h3>Welcome To <?php echo $this->session->flashdata('customer_name'); ?></h3>
-            
-            <p>Hi <?php echo $this->session->flashdata('customer_name'); ?>, You Successfully Registration Our Site
-                Please Go Your Email <a href="mailto:<?php echo $this->session->flashdata('customer_email'); ?>"><b><?php echo $this->session->flashdata('customer_email'); ?></b></a>
-                And Active Your Account . Thank You Stay With Us.
+
+            <p>Hi <?php echo $this->session->flashdata('customer_name'); ?>, You Successfully Registered
+                Please Go to Your Email <a href="mailto:<?php echo $this->session->flashdata('customer_email'); ?>"><b><?php echo $this->session->flashdata('customer_email'); ?></b></a>
+                And Activate Your Account . Thank You Stay With Us.
             </p>
-            
-            
-            
-        </div>  	
+
+
+
+        </div>
         <div class="clear"></div>
     </div>
 </div>
